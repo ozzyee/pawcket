b15cea71a1aeb9af0324163ca7d144387af7e7e9
Kagublanca <reecekagu@gmail.com>
2022-02-18 14:13:35 +0000

    Needs SVG's!

--- src/components/navbar-icon/navbaricon.component.tsx
+++ src/components/navbar-icon/navbaricon.component.tsx
@@ -1,9 +1,10 @@
 import { TNavbarIconProps } from "./navbaricon.definition";
 import Link from "next/link";
+import Image from "next/link";
 
 export default function NavbarIcon({name, direct, onClick} : TNavbarIconProps){
     return (
         <Link href={direct} passHref>
-    <li onClick={onClick}>Icon: {name}</li>
+    <li><Image src={name} alt="A picture" width={500} height={500}/></li>
     </Link>
     )}

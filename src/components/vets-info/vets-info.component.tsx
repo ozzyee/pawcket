e738d3dbee0c6f3007c2bee3187262abc0a37168
ozzyee <62845796+ozzyee@users.noreply.github.com>
2022-02-21 11:45:04 +0000

    vets info component is all done

---
+++ src/components/vets-info/vets-info.component.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { TVetsInfoProps } from "./vets-info.definition";
+import * as S from "./vets-info.style";
+import { Text } from "../text/text.component";
+import VetIcon from "./vet-icon.svg";
+
+export function VetsInfo({
+   className,
+   vetName,
+   vetPhoneNumber,
+   vetAddress,
+   vetWebsite,
+}: TVetsInfoProps) {
+   return (
+      <S.Wrapper>
+         <S.VetsLogoHolder>
+            <VetIcon />
+         </S.VetsLogoHolder>
+         <S.VetsInfoDiv className={className}>
+            <S.VetsInfo>
+               <Text textType="h3" className="vet-name">
+                  {vetName}
+               </Text>
+               <S.TextHolder>
+                  <Text className="contact-info bold">Phone:</Text>
+                  <Text className="contact-info">{vetPhoneNumber}</Text>
+               </S.TextHolder>
+               <S.TextHolder>
+                  <Text className="contact-info bold">Address:</Text>
+                  <Text className="contact-info">{vetAddress}</Text>
+               </S.TextHolder>
+               <S.TextHolder>
+                  <Text className="contact-info bold">Website:</Text>
+                  <Text className="contact-info">{vetWebsite}</Text>
+               </S.TextHolder>
+            </S.VetsInfo>
+         </S.VetsInfoDiv>
+      </S.Wrapper>
+   );
+}
---
+++ src/components/vets-info/vets-info.definition.ts
@@ -0,0 +1,7 @@
+export type TVetsInfoProps = {
+   className?: string;
+   vetName: string;
+   vetPhoneNumber: string;
+   vetAddress: string;
+   vetWebsite: string;
+};

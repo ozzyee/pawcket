99f2070c2d4dff43b53e4173e47b33c0c4aede4d
Mirella <mirellacodes@gmail.com>
2022-02-21 11:02:23 +0000

    updated styling

--- src/components/form-area/form-area.style.ts
+++ src/components/form-area/form-area.style.ts
@@ -1,9 +1,11 @@
 import styled from "styled-components";
+import styles from "../../styles/style.config.json";
 
 export const Area = styled.textarea`
    width: 600px;
    height: 120px;
-   border: 3px solid #cccccc;
+   border: 3px solid ${styles.colors.black};
    padding: 5px;
    font-size: 1em;
+   opacity: 0.7;
 `;

fd861be4daf4bcbc834e5f3d7f6653f8730af115
ozzyee <62845796+ozzyee@users.noreply.github.com>
2022-02-26 18:21:48 +0000

    good to murge

--- src/layouts/create-profiles-form/create-profiles.component.tsx
+++ src/layouts/create-profiles-form/create-profiles.component.tsx
@@ -6,59 +6,109 @@ import { Separator } from "../../components/separator/separator.component";
 import { TCreateProfileProps } from "./create-profiles.definition";
 import * as S from "./create-profiles.style";
 
-export function CreateProfileForm({topTitle, bottomTitle, background, foreground,separatorText,isPet}: TCreateProfileProps){
+export function CreateProfileForm({
+   topTitle,
+   bottomTitle,
+   background,
+   foreground,
+   separatorText,
+   isPet,
+}: TCreateProfileProps) {
+   return (
+      <MainLayout
+         topTitle={topTitle}
+         bottomTitle={bottomTitle}
+         topChildren={<Frame background={background} foreground={foreground} />}
+      >
+         {isPet ? (
+            <>
+               <a style={S.SkipStyle} href="">
+                  Skip
+               </a>
+               <>
+                  <form>
+                     <span>
+                        <FormInputs
+                           placeholder="Name"
+                           inputType="input"
+                           onChange={undefined}
+                        />
+                        <FormInputs
+                           placeholder="Bio"
+                           inputType="text-area"
+                           onChange={undefined}
+                        />
+                     </span>
 
-    
-    return(
-        
-        <MainLayout
-        topTitle={topTitle}
-        bottomTitle={bottomTitle}
-        topChildren={<Frame background={background} foreground={foreground}/>}
-        >
-        <a style={S.SkipStyle} href="">
-            Skip
-        </a>
-            { isPet ?
-                <>
-                <form>
-                    <span>
-                        <FormInputs placeholder="Name" inputType="input"></FormInputs>
-                        <FormInputs placeholder="Bio" inputType="text-area"></FormInputs>
-                    </span>
-                        <Separator separatorText={separatorText} />
-                        <FormInputs placeholder="Sex" inputType="input"></FormInputs>
-                        <FormInputs placeholder="Date of Birth" inputType="input"></FormInputs>
-                        <FormInputs placeholder="Species" inputType="input"></FormInputs>
-                        <FormInputs placeholder="Personality" inputType="input"></FormInputs>
-                        <FormInputs placeholder="Medication" inputType="input"></FormInputs>
-                        <FormInputs placeholder="Weight" inputType="input"></FormInputs>
-                        <FormInputs placeholder="Extra Info" inputType="text-area"></FormInputs>
-                </form>
-                <div style={S.ButtonsWrapper}>
-                    <Buttons dark={false} children="Continue"></Buttons>
-                    <Buttons dark={true} children="Add Another"></Buttons>
-                </div>
-                </>
-                :
-                <>
-                <form>
-                <Separator separatorText={separatorText} />
-                    <span>
-                        <FormInputs placeholder="Name"></FormInputs>
-                        <FormInputs placeholder="Date of Birth"></FormInputs>
-                    </span>
-                        <FormInputs placeholder="Postal Code"></FormInputs>
-                        <FormInputs placeholder="Telephone"></FormInputs>
-                        <FormInputs placeholder="Username"></FormInputs>
-                        <FormInputs placeholder="Extra Info" inputType="text-area"></FormInputs>
-                </form>
-                <div style={S.ButtonsWrapper}>
-                    <Buttons dark={false} children="Continue"></Buttons>
-                </div>
-                </>
-            }
+                     <Separator separatorText={separatorText} />
+                     <FormInputs
+                        placeholder="Sex"
+                        inputType="input"
+                        onChange={undefined}
+                     />
 
-        </MainLayout>
-    )
+                     <FormInputs
+                        placeholder="Date of Birth"
+                        inputType="input"
+                        onChange={undefined}
+                     />
+                     <FormInputs
+                        placeholder="Species"
+                        inputType="input"
+                        onChange={undefined}
+                     />
+                     <FormInputs
+                        placeholder="Personality"
+                        inputType="input"
+                        onChange={undefined}
+                     />
+                     <FormInputs
+                        placeholder="Medication"
+                        inputType="input"
+                        onChange={undefined}
+                     />
+                     <FormInputs
+                        placeholder="Weight"
+                        inputType="input"
+                        onChange={undefined}
+                     />
+                     <FormInputs
+                        placeholder="Extra Info"
+                        inputType="text-area"
+                        onChange={undefined}
+                     />
+                  </form>
+                  <div style={S.ButtonsWrapper}>
+                     <Buttons dark={false}>Continue</Buttons>
+                     <Buttons dark={true}>Add Another</Buttons>
+                  </div>
+               </>
+            </>
+         ) : (
+            <>
+               <form>
+                  <Separator separatorText={separatorText} />
+                  <span>
+                     <FormInputs placeholder="Name" onChange={undefined} />
+                     <FormInputs
+                        placeholder="Date of Birth"
+                        onChange={undefined}
+                     />
+                  </span>
+                  <FormInputs placeholder="Postal Code" onChange={undefined} />
+                  <FormInputs placeholder="Telephone" onChange={undefined} />
+                  <FormInputs placeholder="Username" onChange={undefined} />
+                  <FormInputs
+                     placeholder="Extra Info"
+                     inputType="text-area"
+                     onChange={undefined}
+                  />
+               </form>
+               <div style={S.ButtonsWrapper}>
+                  <Buttons dark={false}>Continue</Buttons>
+               </div>
+            </>
+         )}
+      </MainLayout>
+   );
 }

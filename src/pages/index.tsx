1723832f54c5d15592db602e537edffc83e27b86
ozzyee <62845796+ozzyee@users.noreply.github.com>
2022-03-02 20:35:07 +0000

    feat: is login in redirect

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,13 @@
-import type { NextPage } from "next";
+import type { NextApiRequest, NextPage } from "next";
 import { MainLayout } from "../layouts/main-layout/main-layout.component";
 import Logo from "../../public/dummy-logo.svg";
 import { Buttons } from "../components/buttons/buttons.component";
 import { LandingPage, TextHolder } from "../styles/global.style";
 import { Text } from "../components/text/text.component";
 import { useRouter } from "next/router";
+import { doc, getDoc } from "@firebase/firestore";
+import { AuthService } from "../lib/auth-service/auth.service";
+import { firestoreDB } from "../lib/firebase/firebase.initialize";
 
 const Home: NextPage = () => {
    const router = useRouter();
@@ -44,5 +47,40 @@ const Home: NextPage = () => {
       </>
    );
 };
+
+export async function getServerSideProps({ req }: { req: NextApiRequest }) {
+   try {
+      const cookieRefreshToken = req.cookies.token;
+      const authService = new AuthService();
+      const dataRes = await authService.getFirebaseUserToken(
+         cookieRefreshToken
+      );
+      // const userUID = dataRes.getIdToken.user_id;
+      // const docRef = doc(firestoreDB, "pets", userUID);
+      // const docSnap = await getDoc(docRef);
+      // const _data = docSnap.data();
+
+      console.log(dataRes);
+
+      // No user then send to login/ sign up page
+      if (dataRes) {
+         return {
+            redirect: {
+               destination: "/create-user",
+            },
+         };
+      }
+
+      return {
+         props: {},
+      };
+   } catch (err) {
+      console.log("ERR", err);
+
+      return {
+         props: {},
+      };
+   }
+}
 
 export default Home;

5e0bd8c60f5038ba0e907a4086a0cf91b0917156
Israel <93683580+blueishegg@users.noreply.github.com>
2022-03-02 11:10:05 +0000

    fixed calendar on pet form

--- dummy-data/dummy-data.ts
+++ dummy-data/dummy-data.ts
@@ -1,57 +1,49 @@
-export type TPet = {
-    name?: string;
-    bio?: string;
-    sex?: string;
-    dateOfBirth?: any;
-    personality?: string;
-    medications?:string;
-    weight?: string;
-    aboutMe?: string;
-    profilePic?: string;
-};
+import { TPet } from "../src/layouts/creat-pet-form/creat-pet-form.definition";
 
 export type TUser = {
-    name?: string,
-    address?: string,
-    dateOfBirth?: string,
-    telephone?: string,
-    username?:string,
-    extraInfo?: string,
-    profilePic?: string,
-    pets?: TPet[]
-}
+   name?: string;
+   address?: string;
+   dateOfBirth?: string;
+   telephone?: string;
+   username?: string;
+   extraInfo?: string;
+   profilePic?: string;
+   pets?: TPet[];
+};
 
 export const freddie: TPet = {
-    name: "Freddie",
-    bio: "Did you know that I have been considered a frisbee champion for my mad fetch skills? If you love your frisbee as much as I love mine, you should hit me up so we can be a champion team",
-    sex: "Male",
-    dateOfBirth: "12 of November 2021",
-    personality: "Playful",
-    medications: "N/A",
-    weight: "6 Kilograms",
-    aboutMe: "Freddie loves playing with his stuffedbear and ragging around the kitchen. It's so funny",
-    profilePic: "/dummy/freddie.jpg",
-}
+   name: "Freddie",
+   bio: "Did you know that I have been considered a frisbee champion for my mad fetch skills? If you love your frisbee as much as I love mine, you should hit me up so we can be a champion team",
+   sex: "Male",
+   dateOfBirth: "12 of November 2021",
+   personality: "Playful",
+   medications: "N/A",
+   weight: "6 Kilograms",
+   aboutMe:
+      "Freddie loves playing with his stuffedbear and ragging around the kitchen. It's so funny",
+   profilePic: "/dummy/freddie.jpg",
+};
 
 export const tony: TPet = {
-    name: "Tony",
-    bio: "I'm 3 months old but that doesn't stop me. You better wear shoes or prepare yourself for a toe-hunting session with me.",
-    sex: "Male",
-    dateOfBirth: "5 of December 2021",
-    personality: "Shy",
-    medications: "N/A",
-    weight: "200 Grams",
-    aboutMe: "Tony is too young to have other cat friends but he'll love to meet you once he put on some grams on!!",
-    profilePic: "/dummy/tony.jpg",
-}
+   name: "Tony",
+   bio: "I'm 3 months old but that doesn't stop me. You better wear shoes or prepare yourself for a toe-hunting session with me.",
+   sex: "Male",
+   dateOfBirth: "5 of December 2021",
+   personality: "Shy",
+   medications: "N/A",
+   weight: "200 Grams",
+   aboutMe:
+      "Tony is too young to have other cat friends but he'll love to meet you once he put on some grams on!!",
+   profilePic: "/dummy/tony.jpg",
+};
 
 export const jennifer: TUser = {
-    name: "Jennifer Jones",
-    address: "123 Street Rd.",
-    dateOfBirth: "29 of February 1988",
-    telephone: "N/A",
-    username:"Jennifer J.",
-    extraInfo: "I'm thecnically 8 years old.",
-    profilePic: "",
-    pets: [tony, freddie]
-}
+   name: "Jennifer Jones",
+   address: "123 Street Rd.",
+   dateOfBirth: "29 of February 1988",
+   telephone: "N/A",
+   username: "Jennifer J.",
+   extraInfo: "I'm thecnically 8 years old.",
+   profilePic: "",
+   pets: [tony, freddie],
+};
--- src/layouts/creat-pet-form/creat-pet-form.definition.ts
+++ src/layouts/creat-pet-form/creat-pet-form.definition.ts
@@ -1,3 +1,15 @@
 export type TCreatePetFormProps = {
-  className?: string
-}
+   className?: string;
+};
+
+export type TPet = {
+   name?: string;
+   bio?: string;
+   sex?: string;
+   dateOfBirth?: Date | string;
+   personality?: string;
+   medications?: string;
+   weight?: string;
+   aboutMe?: string;
+   profilePic?: string;
+};

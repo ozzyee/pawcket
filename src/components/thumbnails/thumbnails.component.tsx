0259da28b9c8bc4c717e3c09e8b8f491cff03c1f
Mirella <mirellacodes@gmail.com>
2022-03-10 10:54:06 +0000

    thumbnail working

--- src/components/thumbnails/thumbnails.component.tsx
+++ src/components/thumbnails/thumbnails.component.tsx
@@ -8,35 +8,50 @@ import { RoundImage } from "../round-image/round-img.component";
 import { Text } from "../text/text.component";
 import * as S from "./thumbnails.style";
 
-export function Thumbnails({data, isForPets, isAFriend, userName, className}: TThumbnailsProps){
+export function Thumbnails({
+   data,
+   isForPets,
+   isAFriend,
+   userName,
+   className,
+}: TThumbnailsProps) {
+   if (data === undefined) {
+      return null;
+   }
+   {
+      if (data.length < 1) {
+         if (isForPets) {
+            return (
+               <S.DefaultText>
+                  <Text className="wrapped">
+                     {`${userName} has no pets yet.`}
+                  </Text>
+               </S.DefaultText>
+            );
+         }
 
-    {if(data.length < 1){
-        if(isForPets){
-            return(
-                <S.DefaultText>
-                    <Text className="wrapped">
-                        {`${userName} has no pets yet.`}
-                    </Text>
-                </S.DefaultText>
-            )
-        }
-
-        return(
+         return (
             <S.DefaultText>
-                <Text className="wrapped">
-                    {`${userName} has no friends yet.`}
-                </Text>
+               <Text className="wrapped">
+                  {`${userName} has no friends yet.`}
+               </Text>
             </S.DefaultText>
-        )
-    }}
+         );
+      }
+   }
 
-    {if(isForPets){
-        return(
-            <S.Thumbnails data={data} className={className} isForPets isAFriend={isAFriend}>
-                
-                {data.map((pet:TPet, id:number) => {
-                    return(
-                    <RoundImage
+   {
+      if (isForPets) {
+         return (
+            <S.Thumbnails
+               data={data}
+               className={className}
+               isForPets
+               isAFriend={isAFriend}
+            >
+               {data.map((pet: TPet, id: number) => {
+                  return (
+                     <RoundImage
                         src={pet.profilePic}
                         diameter={120}
                         caption={pet.name}
@@ -48,56 +63,64 @@ export function Thumbnails({data, isForPets, isAFriend, userName, className}: TT
                            })
                         }
                         key={id}
-                    />)
-                    })}
-                {isAFriend ? null :
-                <Buttons
-                       dark={false}
-                       className={!data ? "centeredButton": ""}
-                       onClick={() =>
-                          router.push("/create-pet", undefined, {
-                             shallow: true,
-                          })
-                       }
-                    >
-                       +
-                </Buttons>}
+                     />
+                  );
+               })}
+               {isAFriend ? null : (
+                  <Buttons
+                     dark={false}
+                     className={!data ? "centeredButton" : ""}
+                     onClick={() =>
+                        router.push("/create-pet", undefined, {
+                           shallow: true,
+                        })
+                     }
+                  >
+                     +
+                  </Buttons>
+               )}
             </S.Thumbnails>
-        )
-    }
-    
-    return(
-        <S.Thumbnails data={data} className={className} isForPets isAFriend={isAFriend}>
-            {data.map((user:TUser, id:number) => {
-                return(
-                <RoundImage
-                    src={user.profilePic}
-                    diameter={120}
-                    caption={user.firstName}
-                    className="petPic"
-                    isPet={false}
-                    onClick={() =>
-                       router.push(`/user-profile/${user.id}`, undefined, {
-                          shallow: true,
-                       })
-                    }
-                    key={id}
-                />)
-                })}
-                {isAFriend ? null :
-                <Buttons
-                   dark={false}
-                   className={!data ? "centeredButton": ""}
-                   onClick={() =>
-                      router.push("/create-pet", undefined, {
-                         shallow: true,
-                      })
-                   }
-                >
-                   +
-                </Buttons>}
-        </S.Thumbnails>
-    )
+         );
+      }
 
-    }
+      return (
+         <S.Thumbnails
+            data={data}
+            className={className}
+            isForPets
+            isAFriend={isAFriend}
+         >
+            {data.map((user: TUser, id: number) => {
+               return (
+                  <RoundImage
+                     src={user.profilePic}
+                     diameter={120}
+                     caption={user.firstName}
+                     className="petPic"
+                     isPet={false}
+                     onClick={() =>
+                        router.push(`/user-profile/${user.id}`, undefined, {
+                           shallow: true,
+                        })
+                     }
+                     key={id}
+                  />
+               );
+            })}
+            {isAFriend ? null : (
+               <Buttons
+                  dark={false}
+                  className={!data ? "centeredButton" : ""}
+                  onClick={() =>
+                     router.push("/create-pet", undefined, {
+                        shallow: true,
+                     })
+                  }
+               >
+                  +
+               </Buttons>
+            )}
+         </S.Thumbnails>
+      );
+   }
 }
